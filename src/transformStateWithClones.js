a97c3672dce6da57464ad0e1a42965527c90ba51
szostokm <matiq89@gmail.com>
2025-02-20 19:04:19 +0100

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,22 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let currentState = { ...state }; // Kopia początkowego state
+  const history = []; // Tablica przechowująca historię stanów
+
+  actions.forEach((action) => {
+    if (action.type === 'clear') {
+      currentState = {}; // Tworzymy pusty obiekt
+    } else if (action.type === 'addProperties') {
+      currentState = { ...currentState, ...action.extraData };
+    } else if (action.type === 'removeProperties') {
+      currentState = { ...currentState }; // Tworzymy kopię przed usunięciem
+      action.keysToRemove.forEach((key) => delete currentState[key]);
+    }
+    history.push(currentState); // Zapisujemy aktualny stan
+  });
+
+  return history;
 }
 
 module.exports = transformStateWithClones;
